670883e4ba4e0c287be2d4d5d2497be356c46617
Amjed <com_amjed@yahoo.com>
2015-01-18 22:21:40 +0100

    Add time calculations

--- proactive_query_smartphone_energy.m
+++ proactive_query_smartphone_energy.m
@@ -3,21 +3,44 @@ close;
 clc;
 %labels
 l = {1,2,4,8,16,64,128};
-m = load('data.txt');
-m = m(:,4); % Extract only the energy column
+%%
+%energy calculations
+m = load('data_wifi.txt');
+e = m(:,4); % Extract only the energy column
+t = m(:,2); % Extract only the time column
+t = t/1000; % convert from milliseconds to seconds
 col_len = input('Please enter the number of query per one experiment : ');
-num_of_exper = length(m) / col_len;
-data_vecs = reshape(m  , [col_len , num_of_exper]);
-max_data = max(m);
+num_of_exper = length(e) / col_len;
+e_data_vecs = reshape(e  , [col_len , num_of_exper]);
+t_data_vecs = reshape(t  , [col_len , num_of_exper]);
+e_max_data = max(e);
+t_max_data = max(t);
 % limit number of labels
 l = l(1:num_of_exper);
+
+%%
 %plotting
-bp = boxplot(data_vecs, 'Labels' , l , 'notch' , 'on');
+figure()
+bp_e = boxplot(e_data_vecs, 'Labels' , l , 'notch' , 'on');
+
 grid on
+box on
 xlabel('Number of queries per one request');
 ylabel('Energy (Joule)');
-set(gca , 'YTick' , [0:0.5: max_data]);
+set(gca , 'YTick' , [0:0.5: e_max_data]);
 
 set(findobj(gca,'Type','text'),'FontSize',16)
 set(findobj('type','axes'),'fontsize',16)
-set(bp,'LineWidth',1.5);
+set(bp_e,'LineWidth',1.5);
+
+figure()
+bp_t = boxplot(t_data_vecs, 'Labels' , l , 'notch' , 'on');
+grid on
+box on
+xlabel('Number of queries per one request');
+ylabel('Time (sec)');
+set(gca , 'YTick' , [0:1: t_max_data]);
+
+set(findobj(gca,'Type','text'),'FontSize',16)
+set(findobj('type','axes'),'fontsize',16)
+set(bp_t,'LineWidth',1.5);
